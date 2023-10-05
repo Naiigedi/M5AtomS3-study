9a4b6233ba62e1dc6a3c74ec252e48bf47ca0809
taichi <iromustam@gmail.com>
2023-10-06 01:25:52 +0900

    ADD: button On-Off

---
+++ LED/ButtonON-OFF/ButtonOnOff/ButtonOnOff.ino
@@ -0,0 +1,23 @@
+
+#include "M5AtomS3.h"
+
+const uint8_t LED_PIN = 6;  // LED出力に使用するGPIO PIN番号
+const uint8_t Button_PIN = 7;  // Button入力に使用するGPIO PIN番号
+
+void setup() {
+  //LED出力ピンを出力モードにする
+  pinMode(LED_PIN, OUTPUT); 
+  pinMode(Button_PIN, INPUT_PULLUP);
+  M5.begin(true, true, false,true);
+  digitalWrite(LED_PIN, LOW);       //出力初期化（LED消灯）
+}
+
+void loop() {
+  if(digitalRead(Button_PIN)==0){
+    digitalWrite(LED_PIN, HIGH);
+  } 
+  else{
+    digitalWrite(LED_PIN, LOW);  
+  }
+  delay(100);
+  }
