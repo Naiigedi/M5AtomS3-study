dad1ef64fea51d387ee32f210c6f4660d1ac66ef
taichi <iromustam@gmail.com>
2024-02-19 19:26:06 +0900

    Fix: resolution value

---
+++ ADC/adc.ino
@@ -0,0 +1,17 @@
+#include "M5AtomS3.h"
+
+const uint8_t ADC_PIN = 6;  // LED出力に使用するGPIO PIN番号
+
+void setup() {
+  pinMode(ADC_PIN, INPUT);
+  M5.begin();
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  int analogValue = analogRead(ADC_PIN);
+  
+  M5.Lcd.println(analogValue);
+  delay(2000);
+}
